3f3c1644cfc9df79a8dbd90beb506940778237f4
caner <caneroyku@outlook.com>
2025-12-30 03:59:55 +0300

    path to regexp

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,11 +34,11 @@ app.use("/api/auth", authRoutes);
 
 app.use("/api/message", messageRoutes);
 
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
+  // Use /(.*) for universal wildcard in Express 5+
+  app.get(/^(.*)$/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
